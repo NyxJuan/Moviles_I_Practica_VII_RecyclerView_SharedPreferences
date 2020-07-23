91a0dfbf9c929ed78ee5d4794485085adff32293
Juan Rodriguez <nyxjuan@hotmail.com>
2020-07-23 01:13:32 -0500

    Practica 7 - RecyclerView y SharedPreferences.

--- app/src/main/java/com/example/practica_autores/Detalle.java
+++ app/src/main/java/com/example/practica_autores/Detalle.java
@@ -1,9 +1,22 @@
 package com.example.practica_autores;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import es.dmoral.coloromatic.ColorOMaticDialog;
+import es.dmoral.coloromatic.IndicatorMode;
+import es.dmoral.coloromatic.OnColorSelectedListener;
+import es.dmoral.coloromatic.colormode.ColorMode;
 
 public class Detalle extends AppCompatActivity {
 
@@ -35,6 +48,64 @@ public class Detalle extends AppCompatActivity {
         txtautor.setText("Autor: " +autor);
         txtanio.setText("Publicacion: " +anio);
         txtdescripcion.setText(descripcion);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            cargarColor();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void cargarColor() {
+        new ColorOMaticDialog.Builder()
+                .initialColor(Color.BLACK)
+                .colorMode(ColorMode.ARGB)
+                .indicatorMode(IndicatorMode.HEX)
+                .onColorSelected(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int color){
+                        SharedPreferences sharedPreferences =
+                                Detalle.this.getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor =
+                                sharedPreferences.edit();
+                        editor.putInt(getString(R.string.sp_color_bar),color);
+                        editor.commit();
+                        cambiarColor(color,Detalle.this);
+                    }
+                })
+                .showColorIndicator(true)
+                .create()
+                .show(getSupportFragmentManager(), "ColorOMaticDialog");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        SharedPreferences sharedPreferences = Detalle.this.getPreferences(Context.MODE_PRIVATE);
+        int color = sharedPreferences.getInt(getString(R.string.sp_color_bar),0);
+        if (color != 0){
+            cambiarColor(color, this);
+        }
+    }
+
+    public void cambiarColor(int color, Activity activity){
+        Window window = activity.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(color);
+        window.setNavigationBarColor(color);
     }
 
 }
--- app/src/main/java/com/example/practica_autores/MainActivity.java
+++ app/src/main/java/com/example/practica_autores/MainActivity.java
@@ -4,16 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
+
+import es.dmoral.coloromatic.ColorOMaticDialog;
+import es.dmoral.coloromatic.IndicatorMode;
+import es.dmoral.coloromatic.OnColorSelectedListener;
+import es.dmoral.coloromatic.colormode.ColorMode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -91,5 +103,60 @@ public class MainActivity extends AppCompatActivity {
         }
 
      */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            cargarColor();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void cargarColor() {
+        new ColorOMaticDialog.Builder()
+                .initialColor(Color.BLACK)
+                .colorMode(ColorMode.ARGB)
+                .indicatorMode(IndicatorMode.HEX)
+                .onColorSelected(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int color){
+                        SharedPreferences sharedPreferences =
+                                MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor =
+                                sharedPreferences.edit();
+                        editor.putInt(getString(R.string.sp_color_bar),color);
+                        editor.commit();
+                        cambiarColor(color,MainActivity.this);
+                    }
+                })
+                .showColorIndicator(true)
+                .create()
+                .show(getSupportFragmentManager(), "ColorOMaticDialog");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        SharedPreferences sharedPreferences = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        int color = sharedPreferences.getInt(getString(R.string.sp_color_bar),0);
+        if (color != 0){
+            cambiarColor(color, this);
+        }
+    }
+
+    public void cambiarColor(int color, Activity activity){
+        Window window = activity.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(color);
+        window.setNavigationBarColor(color);
+    }
 
 }
